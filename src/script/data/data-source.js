ca77b273b871d58f4821c7f47ed8a31f53bc5fb6
muflihun <muflihun@firmanedam19@gmail.com>
2020-04-18 22:57:46 +0700

    First Commit

---
+++ src/script/data/data-source.js
@@ -0,0 +1,14 @@
+const DataSource = (onSuccess, onFailed) => {
+    this.onSuccess = onSuccess;
+    this.onFailed = onFailed;
+}
+
+DataSource.prototype.searchClub = keyword => {
+    const filteredClubs = clubs.filter( club => club.name.toUpperCase().includes(keyword.toUpperCase()) );
+
+    if (filteredClubs.length) {
+        this.onSuccess(filteredClubs);
+    } else {
+        this.onFailed('${keyword} is not found');
+    }
+};
